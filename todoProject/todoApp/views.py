5ca9bebd1b3d834261a469584c216a3dca2dd900
alishker <alinoorshukri7@gmail.com>
2020-04-17 12:17:06 +0300

    complete main functinality

--- todoProject/todoApp/views.py
+++ todoProject/todoApp/views.py
@@ -17,3 +17,27 @@ def index(request):
 
     context = {'task': task, 'form': form}
     return render(request, 'todoApp/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Tasks.objects.get(id=pk)
+
+    form = TodoForm(instance=task)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'todoApp/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Tasks.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'todoApp/delete.html', context)
